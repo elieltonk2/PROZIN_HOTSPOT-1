057b7ede68ed18b7def9cfe24329114cfd583f0e
elieltonk2 <146467618+elieltonk2@users.noreply.github.com>
2026-03-03 14:57:30 -0400

    Update vite.config.ts
    12

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({mode}) => {
-  const env = loadEnv(mode, '.', '');
-  return {
-    plugins: [react(), tailwindcss()],
-    define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-      hmr: process.env.DISABLE_HMR !== 'true',
-      allowedHosts: true,
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: true, // Permite rodar em qualquer domínio
+    port: 3000
+  }
 });
