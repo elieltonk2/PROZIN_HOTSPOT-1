9ccd1867be244cdf9aa94976f29a1f5ae9337a3d
elieltonk2 <146467618+elieltonk2@users.noreply.github.com>
2026-03-03 12:07:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,25 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
 import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
-  },
-  server: {
-    host: '0.0.0.0',
-    allowedHosts: 'all'
-  },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      hmr: process.env.DISABLE_HMR !== 'true',
+      allowedHosts: true, // Libera o acesso para o Railway
+    },
+  };
 });
